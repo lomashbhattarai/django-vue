25d4b17210cf925830af1a1389fd67c8956666e6
lomashbhattarai <lomashbhattarai1@gmail.com>
2019-04-14 19:54:54 +0545

    delte virtual environment

--- myproject/Vmyproject/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/lomash/web development/projects/myproject/Vmyproject/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
